ae1e0b01c067bb53fdb9fa893f3419315c80deaa
andres <andresadar@hotmail.com>
2021-02-18 22:23:02 -0500

    Second commit

--- dipci-main/dipci/lib/screens/welcome.dart
+++ dipci-main/dipci/lib/screens/welcome.dart
@@ -1,4 +1,6 @@
+import 'package:dipci/colors/color.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 import '../images/images.dart';
@@ -20,24 +22,73 @@ class WelcomePage extends StatelessWidget {
             child: Column(
               children: [
                 Expanded(
-                  flex: 3,
+                  flex: 4,
                   child: PageView(
                     controller: _controller,
                     physics: BouncingScrollPhysics(),
                     children: [
-                      WelcomePages(Colors.red, _controller),
-                      WelcomePages(Colors.blue, _controller),
-                      WelcomePages(Colors.green, _controller),
+                      WelcomePages(
+                        color: Colors.white,
+                        image: imagenes.pig,
+                        text:
+                            'Hola esto es una prueba, hola esto es una prueba',
+                        textColor: Colors.black,
+                        textSize: 20.0,
+                      ),
+                      WelcomePages(
+                        color: Colors.white,
+                        image: imagenes.bear,
+                        text:
+                            'Hola esto es una prueba 2, esto es una prueba 2 2 2',
+                        textColor: Colors.black,
+                        textSize: 20.0,
+                      ),
+                      WelcomePages(
+                        color: Colors.white,
+                        image: imagenes.creditTarget,
+                        text: 'Bienvenido',
+                        textColor: colorPrincipal,
+                        textSize: 45.0,
+                      )
                     ],
                   ),
                 ),
                 Expanded(
                   flex: 1,
-                  child: Center(
-                    child: SmoothPageIndicator(
-                      controller: _controller,
-                      count: 3,
-                      effect: WormEffect(),
+                  child: Container(
+                    color: Colors.white,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Center(
+                          child: SmoothPageIndicator(
+                            controller: _controller,
+                            count: 3,
+                            effect: WormEffect(),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 20.0),
+                          child: Align(
+                            alignment: Alignment.bottomRight,
+                            child: RaisedButton(
+                              color: colorPrincipal,
+                              shape: StadiumBorder(),
+                              child: Text(
+                                'Siguiente',
+                                style: TextStyle(
+                                    fontSize: 18, color: Colors.white),
+                              ),
+                              onPressed: () {
+                                _controller.nextPage(
+                                    duration: Duration(microseconds: 500),
+                                    curve: Curves.bounceIn);
+                              },
+                            ),
+                          ),
+                        )
+                      ],
                     ),
                   ),
                 ),
@@ -52,20 +103,43 @@ class WelcomePage extends StatelessWidget {
 
 class WelcomePages extends StatelessWidget {
   final color;
-  final _controller;
+  final image;
+  final text;
+  final textColor;
+  final textSize;
 
-  const WelcomePages(this.color, this._controller);
+  const WelcomePages(
+      {this.color, this.image, this.text, this.textColor, this.textSize});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: this.color,
-      ),
       body: Container(
         height: double.infinity,
         width: double.infinity,
         color: this.color,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              margin: EdgeInsets.all(50.0),
+              height: 280,
+              width: 280,
+              child: SvgPicture.asset(image),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 25.0),
+              child: Text(
+                this.text,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: textColor,
+                    fontSize: textSize), //TODO: CAMBIAR LA TIPOGRAFIA
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
